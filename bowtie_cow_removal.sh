09a83d9867415ae5bd9364be876db06fa4b72213
Amulya Baral <amulya.baral@nmbu.no>
2024-08-26 15:40:18 +0200

    updated

--- bowtie_cow_removal.sh
+++ bowtie_cow_removal.sh
@@ -12,5 +12,5 @@ module load Bowtie2
 for forward_read in $(find /mnt/project/AntibiotiKU/hunt_rawfiles/ -type f -name "*_1.fq.gz"); do 
     reverse_read="${forward_read/_1/_2}" 
     sample_name="$(basename "$forward_read" _1.fq.gz)" 
-    bowtie2 -x /mnt/project/AntibiotiKU/bowtie_indexes/ARS-UCD1.2 -p 64 -1 $forward_read -2 $reverse_read --un-conc-gz /mnt/project/AntibiotiKU/hunt_host_removed/$sample_name.host_removed.fq ;   
+    bowtie2 -x /mnt/project/AntibiotiKU/bowtie_indexes/ARS-UCD1.2 -p 64 -1 $forward_read -2 $reverse_read --un-conc-gz /mnt/project/AntibiotiKU/hunt_host_removed/$sample_name.host_removed.fq.gz ;   
 done
