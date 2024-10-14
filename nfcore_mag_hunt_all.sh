f8f8d36bc673c875b006c771d10b1deb04024773
Amulya Baral <amulya.baral@nmbu.no>
2024-10-14 17:07:19 +0200

    updated gtdb database in hunt mag script

--- nfcore_mag_hunt_all.sh
+++ nfcore_mag_hunt_all.sh
@@ -10,16 +10,14 @@
 cd /mnt/project/AntibiotiKU/
 
 nextflow run nf-core/mag \
-        -profile singularity \
-        --input /mnt/project/AntibiotiKU/samples.csv \
-        --output /mnt/project/AntibiotiKU/hunt_all_nfmag_output \
-        -c /mnt/project/AntibiotiKU/nextflow.config \
-        --max_cpus 62 \
-        --max_memory 190.GB \
-        --refine_bins_dastool \
-        --kraken2_db /mnt/project/AntibiotiKU/databases/k2_standard_20231009 \
-        --run_virus_identification \
-        --min_contig_size 1000
-
-
+ -profile singularity \
+ --input /mnt/project/AntibiotiKU/samples.csv \
+ --output /mnt/project/AntibiotiKU/hunt_all_nfmag_output \
+ -c /mnt/project/AntibiotiKU/nextflow.config \
+ --refine_bins_dastool \
+ --kraken2_db /mnt/project/AntibiotiKU/databases/k2_standard_20231009 \
+ --run_virus_identification \
+ --min_contig_size 1000 \
+ --gtdb_db https://data.gtdb.ecogenomic.org/releases/release220/220.0/auxillary_files/gtdbtk_package/full_package/gtdbtk_r220_data.tar.gz
+ 
 echo "pipeline complete! yay!"
