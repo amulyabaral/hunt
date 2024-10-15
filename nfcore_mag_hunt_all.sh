514decd67223b79889c155adb5ecb3c48952b6d4
Amulya Baral <amulya.baral@nmbu.no>
2024-10-15 09:13:03 +0200

    updated

--- nfcore_mag_hunt_all.sh
+++ nfcore_mag_hunt_all.sh
@@ -12,12 +12,12 @@ cd /mnt/project/AntibiotiKU/
 nextflow run nf-core/mag \
  -profile singularity \
  --input /mnt/project/AntibiotiKU/samples.csv \
- --output /mnt/project/AntibiotiKU/hunt_all_nfmag_output \
+ --outdir /mnt/project/AntibiotiKU/hunt_all_nfmag_output \
  -c /mnt/project/AntibiotiKU/nextflow.config \
  --refine_bins_dastool \
- --kraken2_db /mnt/project/AntibiotiKU/databases/k2_standard_20231009 \
+ --kraken2_db /mnt/project/AntibiotiKU/databases/k2_standard_20231009/ \
  --run_virus_identification \
  --min_contig_size 1000 \
- --gtdb_db /mnt/project/AntibiotiKU/gtdb_db/gtdbtk_r220_data.tar.gz
+ --gtdb_db /mnt/project/AntibiotiKU/gtdb_db/gtdbtk_r220_data.tar.gz \
 
 echo "pipeline complete! yay!"
