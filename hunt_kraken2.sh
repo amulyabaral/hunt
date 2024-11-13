ddf35979bb989a6cc680a123d8ca980ffb51c8f1
Amulya Baral <amulya.baral@nmbu.no>
2024-11-13 09:52:08 +0100

    updated kraken2 to only run 2 at a time

--- hunt_kraken2.sh
+++ hunt_kraken2.sh
@@ -7,11 +7,11 @@
 #SBATCH --mail-type=ALL
 
 # Find the forward reads and process them in parallel using xargs
-find /mnt/project/AntibiotiKU/trimmed_reads/ -type f -name "*_1.fq.gz" | xargs -P 64 -I {} bash -c '
+find /mnt/project/AntibiotiKU/trimmed_reads/ -type f -name "*_1.fq.gz" | xargs -P 2 -I {} bash -c '
     forward_read="{}"
     reverse_read="$(echo "$forward_read" | sed "s/_1\.fq\.gz$/_2.fq.gz/")"
     sample_name="$(basename "$forward_read" _1.fq.gz)"
-    kraken2 --threads 2 --gzip-compressed --db /mnt/project/AntibiotiKU/databases/k2_standard_20231009 --paired $forward_read $reverse_read --output /mnt/project/AntibiotiKU/hunt_kraken2_output/$sample_name.kraken_out --report /mnt/project/AntibiotiKU/hunt_kraken2_output/$sample_name.kraken_report.txt
+    kraken2 --threads 64 --gzip-compressed --db /mnt/project/AntibiotiKU/databases/k2_standard_20231009 --paired $forward_read $reverse_read --output /mnt/project/AntibiotiKU/hunt_kraken2_output/$sample_name.kraken_out --report /mnt/project/AntibiotiKU/hunt_kraken2_output/$sample_name.kraken_report.txt
 '
 
 
