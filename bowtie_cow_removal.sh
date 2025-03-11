19c96cb4ed0ca0145b3cb68e8fd758eb09cb40db
Amulya Baral <amulyabaral2@gmail.com>
2025-03-11 09:59:39 +0100

    added assembly script

--- bowtie_cow_removal.sh
+++ bowtie_cow_removal.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-#SBATCH --ntasks=64                                 # Number of cores (CPU)
-#SBATCH --nodes=1                                   # Number of nodes to use
-#SBATCH --job-name=hunt_host                  # Job name 
-#SBATCH --mem=300G                                  # Required RAM - Default memory per CPU is 3GB.
-#SBATCH --partition=hugemem-avx2                         # Use smallmem for jobs < 10 GB RAM|Options: hugemen, smallmem, gpu
-#SBATCH --mail-user=amulya.baral@nmbu.no            # Email when job is done.
+#SBATCH --ntasks=64                              # Number of cores (CPU)
+#SBATCH --job-name=hunt_host                     # Job name
+#SBATCH --mem=300G                               # Required RAM - Default memory per CPU is 3GB.
+#SBATCH --mail-user=amulya.baral@nmbu.no         # Email when job is done.
 #SBATCH --mail-type=ALL
 
 module load Bowtie2
 
-for forward_read in $(find /mnt/project/AntibiotiKU/hunt_rawfiles/ -type f -name "*_1.fq.gz"); do 
-    reverse_read="$(echo "$forward_read" | sed 's/_1\.fq\.gz$/_2.fq.gz/')" 
-    sample_name="$(basename "$forward_read" _1.fq.gz)" 
-    echo "processing $sample_name"
-    bowtie2 -x /mnt/project/AntibiotiKU/bowtie_indexes/ARS-UCD1.2/ARS-UCD1.2 -p 64 -1 $forward_read -2 $reverse_read --un-conc-gz /mnt/project/AntibiotiKU/hunt_host_removed/$sample_name.host_removed.fq.gz > /mnt/project/AntibiotiKU/hunt_host_removed/$sample_name.mapped_unmapped.sam ;   
-done
+# Function to process each sample
+process_sample() {
+    forward_read=$1
+    reverse_read=$(echo "$forward_read" | sed 's/_1\.fq\.gz$/_2.fq.gz/')
+    sample_name=$(basename "$forward_read" _1.fq.gz)
+    echo "Processing $sample_name"
+    bowtie2 -x /mnt/project/AntibiotiKU/bowtie_indexes/ARS-UCD1.2/ARS-UCD1.2 -p 8 -1 $forward_read -2 $reverse_read --un-conc-gz /mnt/project/AntibiotiKU/hunt_host_removed_trimmed/$sample_name.host_removed.fq.gz > /mnt/project/AntibiotiKU/hunt_host_removed_trimmed/$sample_name.mapped_unmapped.sam
+}
+
+export -f process_sample
+
+# Find all forward reads and pass them to xargs
+find /mnt/project/AntibiotiKU/trimmed_reads/ -type f -name "*_1.fq.gz" | xargs -n 1 -P 8 -I {} bash -c 'process_sample "$@"' _ {}
